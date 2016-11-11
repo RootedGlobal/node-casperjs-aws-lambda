6e37a623e3c25d710bd0f6ef144bbd9664eb3ee3
Narain Sagar <narainmenghwar@gmail.com>
2016-11-11 20:30:54 +0500

    fixes.

--- casperjs-script.js
+++ casperjs-script.js
@@ -1,11 +1,17 @@
 // Simple Javascript example
 var casper = require('casper').create();
-var url = 'http://casperjs.org/';
 
-console.log('Loading a web page: ' + url);
+var url1 = 'http://casperjs.org/';
+var url2 = 'http://phantomjs.org';
 
-casper.start(url, function() {
-  this.echo('Page title is: ' + this.getTitle());
+console.log('Loading a web page: ' + url1);
+casper.start(url1, function() {
+    this.echo('Page title is: ' + this.getTitle());
+});
+
+console.log('Loading a web page: ' + url2);
+casper.thenOpen(url2, function() {
+    this.echo('Second Page: ' + this.getTitle());
 });
 
 casper.run();
--- index.js
+++ index.js
@@ -4,47 +4,47 @@ var path = require('path'),
 
 // Get the path to the phantomjs application
 function getPhantomFileName() {
-  var phantomPath = path.join(__dirname, 'node_modules', 'phantomjs', 'bin', 'phantomjs');
-  if (process.env.DEBUG && fs.existsSync(phantomPath)) {
-    return phantomPath;
-  }
-  return path.join(__dirname, 'phantomjs');
+    var phantomPath = path.join(__dirname, 'node_modules', 'phantomjs', 'bin', 'phantomjs');
+    if ( /* process.env.DEBUG && */ fs.existsSync(phantomPath)) {
+        return phantomPath;
+    }
+    return path.join(__dirname, 'phantomjs');
 }
 
 // Call the casperJS script
 function runCasper(scriptName, callback) {
-  var casperPath = path.join(__dirname, 'node_modules', 'casperjs', 'bin', 'casperjs');
-  var outputData = [];
-  var error = null;
-  var childArgs = [
-    path.join(__dirname, scriptName)
-  ];
-  var childOptions = {
-    'PHANTOMJS_EXECUTABLE': getPhantomFileName()
-  };
+    var casperPath = path.join(__dirname, 'node_modules', 'casperjs', 'bin', 'casperjs');
+    var outputData = [];
+    var error = null;
+    var childArgs = [
+        path.join(__dirname, scriptName)
+    ];
+    var childOptions = {
+        'PHANTOMJS_EXECUTABLE': getPhantomFileName()
+    };
 
-  console.log('Calling casperJS: ', casperPath, childArgs, childOptions);
+    console.log('Calling casperJS: ', casperPath, childArgs, childOptions);
 
-  var ps = childProcess.execFile(casperPath, childArgs, childOptions);
+    var ps = childProcess.execFile(casperPath, childArgs, childOptions);
 
-  ps.stdout.on('data', function (data) {
-    console.log(data);
-    outputData.push(data);
-  });
+    ps.stdout.on('data', function(data) {
+        console.log(data);
+        outputData.push(data);
+    });
 
-  ps.stderr.on('data', function (data) {
-    console.log('casper error  ---:> ' + data);
-    error = new Error(data);
-  });
+    ps.stderr.on('data', function(data) {
+        console.log('casper error  ---:> ' + data);
+        error = new Error(data);
+    });
 
-  ps.on('exit', function (code) {
-    console.log('child process exited with code ' + code);
-    callback(error, outputData);
-  });
+    ps.on('exit', function(code) {
+        console.log('child process exited with code ' + code);
+        callback(error, outputData);
+    });
 }
 
 // Entry Point
-exports.handler = function (event, context) {
-  // Execute the casperJS call and exit
-  runCasper('casperjs-script.js', context.done);
+exports.handler = function(event, context) {
+    // Execute the casperJS call and exit
+    runCasper('casperjs-script.js', context.done);
 };
